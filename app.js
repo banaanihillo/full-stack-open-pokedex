2cc91bfb4cbba0a0086dd9b09e5a1d6881c95ae1
vjhmur@utu.fi <banaanihillo@protonmail.ch>
2021-05-03 14:14:38 +0300

    Added exercise11-6
    Commented out the nonexistent port,
    since no environment variables are configured,
    also fixed the log command,
    to log the port instead of hard-coded 5000

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+// but dotenv is not even in use yet,
+// so process does not exist
+const PORT = /*process.env.PORT || */5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000')
+  console.log(`Server started on port ${PORT}`)
 })

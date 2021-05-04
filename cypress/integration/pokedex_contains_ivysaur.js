7926aa9b6375acc716937fa2cb9a6aae8addd166
vjhmur@utu.fi <banaanihillo@protonmail.ch>
2021-05-04 16:20:50 +0300

    Added cypress
    Set up cypress,
    also added a simple end to end test,
    to see whether Ivysaur is present,
    and it sure is

---
+++ cypress/integration/pokedex_contains_ivysaur.js
@@ -0,0 +1,9 @@
+describe("Pokédex", () => {
+    it("front page can be opened", () => {
+        cy.visit("http://localhost:8080")
+        cy.contains("ivysaur")
+        cy.contains(
+            "Pokémon and Pokémon character names are trademarks of Nintendo."
+        )
+    })
+})

7a431870d506d13c79f8cbd9d50d64510f2eebb0
vjhmur@utu.fi <banaanihillo@protonmail.ch>
2021-05-05 16:59:50 +0300

    Added AkhileshNS heroku deployment
    Added deployment into the workflow,
    also added the required Procfile,
    and modified the port again,
    into using the heroku environment variable

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-// but dotenv is not even in use yet,
-// so process does not exist
-const PORT = /*process.env.PORT || */5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
